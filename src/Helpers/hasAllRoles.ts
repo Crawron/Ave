7b930d4e12af2c39fc948647bd16d3739242853f
Crawron <crawronbird@gmail.com>
2018-07-29 13:03:47 -0500

    Added hasAllRoles helper

---
+++ src/Helpers/hasAllRoles.ts
@@ -0,0 +1,12 @@
+import { GuildMember, RoleResolvable, Role } from "discord.js"
+
+function hasAllRoles(member: GuildMember, roles: RoleResolvable[]) {
+    let response = true
+    roles.forEach((role) => {
+        const isId = typeof role === "string"
+        const roleId = isId ? <string>role : (<Role>role).id
+        const hasRole = member.roles.has(roleId)
+        if (!hasRole) response = false
+    })
+    return response
+}
