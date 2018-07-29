c49a9d09e3d73fc12cede54cdbd7f83f66ff95d2
Crawron <crawronbird@gmail.com>
2018-07-29 13:43:16 -0500

    WIP example bot

---
+++ src/test/Commands/Hi.ts
@@ -0,0 +1,18 @@
+import { Message, Client, User } from "discord.js"
+import { Command, requiresRoles } from "../../main"
+
+const serverAdminRoleId = "421565847815979018"
+
+export default class extends Command {
+    constructor(client: Client) {
+        super(client, {
+            name: "hi",
+            description: "Test command.",
+        })
+    }
+
+    @requiresRoles([serverAdminRoleId])
+    async run(msg: Message, user: User) {
+        return msg.channel.send(`Hello, ${user}!`)
+    }
+}
---
+++ src/test/bot.ts
@@ -0,0 +1,22 @@
+import { Client } from "discord.js"
+import { CommandError } from "../main"
+
+import Hi from "./Commands/Hi"
+
+const client = new Client()
+const hi = new Hi(client)
+
+client.on("message", (msg) => {
+    if (!msg.author.bot) {
+        try {
+            hi.run(msg, msg.author)
+        } catch (e) {
+            if (e instanceof CommandError) {
+                msg.channel.send(e.message)
+                msg.channel.send("```" + e.command.name + "```")
+            }
+        }
+    }
+})
+
+client.login()
